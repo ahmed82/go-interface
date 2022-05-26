76e95fa1e5e1c93ceaa355a26cb87d99e789a222
Ahmed Al Salih <ahmed82@users.noreply.github.com>
2022-05-26 11:35:37 -0400

    implement multiable interface

--- main.go
+++ main.go
@@ -5,10 +5,25 @@ import "fmt"
 type Printer interface {
 	Print() string
 }
-type myPringter struct{}
 
-func (mp myPringter) Print() string {
+type Scanner interface {
+	Scan() string
+}
+
+//Composit interface that combain TWO interfaces
+type PrinterScanner interface {
+	Printer
+	Scanner
+}
+
+type myPrinter struct{}
+
+func (mp myPrinter) Print() string {
 	return "Printing One page ..."
+}
+
+func (mp myPrinter) Scan() string {
+	return "Scanned One page ..."
 }
 
 type scondPrinter struct{}
@@ -17,12 +32,18 @@ func (sp scondPrinter) Print() string {
 	return "Printing More pages >>>"
 }
 
+func (sp scondPrinter) Scan() string {
+	return "Scanned More pages >>>"
+}
+
 //func process(equipment myPringter) {
-func process(equipment Printer) {
+//func process(equipment Printer) {
+func process(equipment PrinterScanner) {
 	fmt.Println("Running Print,,,", equipment.Print())
+	fmt.Println("Running Scan,,,", equipment.Scan())
 }
 func main() {
-	printer := myPringter{}
+	printer := myPrinter{}
 	otherPrinter := scondPrinter{}
 	process(printer)
 	process(otherPrinter)
